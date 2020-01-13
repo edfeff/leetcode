f3eee673527851ed02a021f3ce9fb943eae33d49
pengpeng.wang <pengpeng.wang@analyticservice.net>
2020-01-13 10:06:20 +0800

    add  最大子数组之和

--- src/main/java/com/wpp/RecursiveStudy.java
+++ src/main/java/com/wpp/RecursiveStudy.java
@@ -8,8 +8,12 @@ package com.wpp;
 public class RecursiveStudy {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, -1, -4, -2, -1, 9, 10};
-        System.out.println(new RecursiveStudy().findMax(arr));
-        System.out.println(new RecursiveStudy().sum(arr));
+        int[] arr2 = new int[]{1, 2, 3, 1, 4, 2, 1, 9, 10};
+//        System.out.println(new RecursiveStudy().findMax(arr));
+//        System.out.println(new RecursiveStudy().sum(arr));
+        System.out.println(new RecursiveStudy().maxSubArrSum(arr));
+        System.out.println(new RecursiveStudy().maxSubArrSum(arr2));
+//        System.out.println(new RecursiveStudy().maxCrossingArrSum(arr, 0, 0, 1));
     }
 
 
@@ -48,6 +52,63 @@ public class RecursiveStudy {
         int mid = (low + high) / 2;
         int leftMax = findMax(arr, low, mid);
         int rightMax = findMax(arr, mid + 1, high);
+
+
         return leftMax > rightMax ? leftMax : rightMax;
+    }
+
+    /**
+     * 最大子串和
+     *
+     * @param arr
+     * @return
+     */
+    public int maxSubArrSum(int[] arr) {
+        return maxSubArrSum(arr, 0, arr.length - 1);
+    }
+
+    private int maxSubArrSum(int[] arr, int low, int high) {
+        if (low >= high) {
+            return arr[low];
+        }
+        int mid = (low + high) / 2;
+        int leftMax = maxSubArrSum(arr, low, mid);
+//        System.out.println("left: " + low + "--" + mid + " = " + leftMax);
+
+        int rightMax = maxSubArrSum(arr, mid + 1, high);
+//        System.out.println("right: " + (mid + 1) + "--" + high + " = " + rightMax);
+
+        int midMax = maxCrossingArrSum(arr, low, mid, high);
+//        System.out.println("Cross: " + low + "--" + mid + "--" + high + " = " + midMax);
+
+        int max = leftMax > rightMax ? leftMax : rightMax;
+        max = max > midMax ? max : midMax;
+        return max;
+    }
+
+    /**
+     * 跨越中点的最子数组之和
+     *
+     * @param arr
+     * @param low
+     * @param mid
+     * @param high
+     * @return
+     */
+    private int maxCrossingArrSum(int[] arr, int low, int mid, int high) {
+        int leftMax = Integer.MIN_VALUE;
+        int rightMax = Integer.MIN_VALUE;
+
+        int lSum = 0;
+        for (int i = mid; i >= low; i--) {
+            lSum += arr[i];
+            leftMax = lSum > leftMax ? lSum : leftMax;
+        }
+        int rSum = 0;
+        for (int i = mid + 1; i <= high; i++) {
+            rSum += arr[i];
+            rightMax = rSum > rightMax ? rSum : rightMax;
+        }
+        return leftMax + rightMax;
     }
 }
